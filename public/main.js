c252ac0a5d553e85d3ff0c9a2f700bdf43f0a12d
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-06-16 12:54:28 -1000

    Update

--- public/main.js
+++ public/main.js
@@ -41,38 +41,38 @@ document.getElementById("events").addEventListener("submit", function(event) {
     });
 });
 
-// const refreshButton = document.getElementById('refreshButton');
-// const contentContainer = document.getElementById('content');
+const refreshButton = document.getElementById('refreshButton');
+const contentContainer = document.getElementById('content');
 
-// // Function to fetch and display events in the content container
-// const displayEvents = () => {
-//   // Send an HTTP GET request to fetch events from the server
-//   fetch('/events')
-//     .then(response => response.json())
-//     .then(data => {
-//       // Clear the existing content
-//       contentContainer.innerHTML = '';
+// Function to fetch and display events in the content container
+const displayEvents = () => {
+  // Send an HTTP GET request to fetch events from the server
+  fetch('/events')
+    .then(response => response.json())
+    .then(data => {
+      // Clear the existing content
+      contentContainer.innerHTML = '';
 
-//       // Display each event in the content container
-//       data.forEach(event => {
-//         const eventElement = document.createElement('div');
-//         eventElement.innerHTML = `
-//           <p>Date: ${event.date}</p>
-//           <p>Time: ${event.time}</p>
-//           <p>Activity: ${event.activity}</p>
-//           <p>Location: ${event.location}</p>
-//           <p>Notes: ${event.notes}</p>
-//         `;
-//         contentContainer.appendChild(eventElement);
-//       });
-//     })
-//     .catch(error => {
-//       console.error('Error fetching events:', error);
-//     });
-// };
+      // Display each event in the content container
+      data.forEach(event => {
+        const eventElement = document.createElement('div');
+        eventElement.innerHTML = `
+          <p>Date: ${event.date}</p>
+          <p>Time: ${event.time}</p>
+          <p>Activity: ${event.activity}</p>
+          <p>Location: ${event.location}</p>
+          <p>Notes: ${event.notes}</p>
+        `;
+        contentContainer.appendChild(eventElement);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching events:', error);
+    });
+};
 
 // Add event listener to the refresh button
-// refreshButton.addEventListener('click', displayEvents);
+refreshButton.addEventListener('click', displayEvents);
 
 // Call the displayEvents function initially to load the events
-// displayEvents();
+displayEvents();
