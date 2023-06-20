a675032224863bd986be6ac74c8a09fce6006ec7
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-06-20 05:51:28 -1000

    Update

--- server.js
+++ server.js
@@ -99,5 +99,5 @@ app.delete('/events/:id', async (req, res) => {
   });
 
 app.listen(port, () => {
-    console.log('Server started on port 3000!')
+    console.log(`Server started on port ${port}`)
 });
