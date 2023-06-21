901ce006678742efd249943cc601d31e6650d0e4
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-06-20 20:15:03 -1000

    Update

--- public/main.js
+++ public/main.js
@@ -1,54 +1,58 @@
-// Display Event in List
-const displayEvent = (event) => {
-  const li = document.createElement('li');
-  li.dataset.id = event.id;
-  li.innerHTML = `
-    <strong data-type="time">Time:</strong> ${event.time}<br>
-    <strong>Activity:</strong> ${event.activity}<br>
-    <strong>Location:</strong> ${event.location}<br>
-    <strong>Notes:</strong> ${event.notes}<br>
-    <div>
-      <label for="completed-${event.id}">Completed:</label>
-      <input type="checkbox" id="completed-${event.id}" class="completed-checkbox" data-id="${event.id}">
-    </div>
-    <div class="completion-time" data-id="${event.id}"></div>
-    <button class="delete-button">Delete</button>
-    <button class="update-button">Update</button>
-  `;
+document.addEventListener('DOMContentLoaded', () => {
+  const eventList = document.getElementById('event-list');
 
-  const sortedListItems = Array.from(eventList.getElementsByTagName('li')).sort((a, b) => {
-    const timeA = a.querySelector('strong[data-type="time"]').textContent;
-    const timeB = b.querySelector('strong[data-type="time"]').textContent;
-    return timeA.localeCompare(timeB);
-  });
+  // Display Event in List
+  const displayEvent = (event) => {
+    const li = document.createElement('li');
+    li.dataset.id = event.id;
+    li.innerHTML = `
+      <strong data-type="time">Time:</strong> ${event.time}<br>
+      <strong>Activity:</strong> ${event.activity}<br>
+      <strong>Location:</strong> ${event.location}<br>
+      <strong>Notes:</strong> ${event.notes}<br>
+      <div>
+        <label for="completed-${event.id}">Completed:</label>
+        <input type="checkbox" id="completed-${event.id}" class="completed-checkbox" data-id="${event.id}">
+      </div>
+      <div class="completion-time" data-id="${event.id}"></div>
+      <button class="delete-button">Delete</button>
+      <button class="update-button">Update</button>
+    `;
 
-  const insertIndex = sortedListItems.findIndex((item) => item.dataset.id > event.id);
-  if (insertIndex !== -1) {
-    eventList.insertBefore(li, sortedListItems[insertIndex]);
-  } else {
-    eventList.appendChild(li);
-  }
+    const sortedListItems = Array.from(eventList.getElementsByTagName('li')).sort((a, b) => {
+      const timeA = a.querySelector('strong[data-type="time"]').textContent;
+      const timeB = b.querySelector('strong[data-type="time"]').textContent;
+      return timeA.localeCompare(timeB);
+    });
 
-  const deleteButton = li.querySelector('.delete-button');
-  deleteButton.addEventListener('click', () => deleteEvent(event.id));
+    const insertIndex = sortedListItems.findIndex((item) => item.dataset.id > event.id);
+    if (insertIndex !== -1) {
+      eventList.insertBefore(li, sortedListItems[insertIndex]);
+    } else {
+      eventList.appendChild(li);
+    }
 
-  const updateButton = li.querySelector('.update-button');
-  updateButton.addEventListener('click', () => populateUpdateForm(event.id));
+    const deleteButton = li.querySelector('.delete-button');
+    deleteButton.addEventListener('click', () => deleteEvent(event.id));
 
-  const completedCheckbox = li.querySelector(`#completed-${event.id}`);
-  completedCheckbox.addEventListener('change', () => handleCompletedCheckbox(event.id));
+    const updateButton = li.querySelector('.update-button');
+    updateButton.addEventListener('click', () => populateUpdateForm(event.id));
 
-  const completionTime = li.querySelector(`.completion-time[data-id="${event.id}"]`);
-  if (event.completed) {
-    completedCheckbox.checked = true;
-    completionTime.textContent = `Completed at: ${event.completionTime}`;
-  }
-};
+    const completedCheckbox = li.querySelector(`#completed-${event.id}`);
+    completedCheckbox.addEventListener('change', () => handleCompletedCheckbox(event.id));
 
-// Handle Completed Checkbox
-const handleCompletedCheckbox = async (eventId) => {
-  const checkbox = document.querySelector(`.completed-checkbox[data-id="${eventId}"]`);
-  const completionTime = document.querySelector(`.completion-time[data-id="${eventId}"]`);
+    const completionTime = li.querySelector(`.completion-time[data-id="${event.id}"]`);
+    if (event.completed) {
+      completedCheckbox.checked = true;
+      completionTime.textContent = `Completed at: ${event.completionTime}`;
+    }
+  };
+
+  // Handle Completed Checkbox
+  const handleCompletedCheckbox = async (eventId) => {
+    const checkbox = document.querySelector(`.completed-checkbox[data-id="${eventId}"]`);
+    const completionTime = document.querySelector(`.completion-time[data-id="${eventId}"]`);
+
 
   if (checkbox.checked) {
     const currentTime = new Date().toLocaleTimeString();
@@ -99,3 +103,4 @@ const handleCompletedCheckbox = async (eventId) => {
     }
   }
 };
+});
