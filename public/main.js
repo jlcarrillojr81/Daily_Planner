cc5ddef55cd8ceb38fcc4273d6f9123ab7c8ce45
Joseph Carrillo <jlcarrillojr81@gmail.com>
2023-06-20 19:29:17 -1000

    Update

--- public/main.js
+++ public/main.js
@@ -46,38 +46,60 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
 
     const idInput = document.getElementById('update-id');
-    const timeInput = document.getElementById('update-time');
-    const activityInput = document.getElementById('update-activity');
-    const locationInput = document.getElementById('update-location');
-    const notesInput = document.getElementById('update-notes');
-
-    const eventData = {
-      time: timeInput.value,
-      activity: activityInput.value,
-      location: locationInput.value,
-      notes: notesInput.value,
-    };
 
     try {
-      const response = await fetch(`/events/${idInput.value}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(eventData),
-      });
+      const response = await fetch(`/events/${idInput.value}`);
 
       if (response.status === 200) {
-        const updatedEvent = await response.json();
-        updateEventInList(updatedEvent);
-        idInput.value = '';
-        timeInput.value = '';
-        activityInput.value = '';
-        locationInput.value = '';
-        notesInput.value = '';
+        const eventToUpdate = await response.json();
+
+        const timeInput = document.getElementById('update-time');
+        const activityInput = document.getElementById('update-activity');
+        const locationInput = document.getElementById('update-location');
+        const notesInput = document.getElementById('update-notes');
+
+        timeInput.value = eventToUpdate.time;
+        activityInput.value = eventToUpdate.activity;
+        locationInput.value = eventToUpdate.location;
+        notesInput.value = eventToUpdate.notes;
+
+        updateForm.addEventListener('submit', async (e) => {
+          e.preventDefault();
+
+          const eventData = {
+            time: timeInput.value,
+            activity: activityInput.value,
+            location: locationInput.value,
+            notes: notesInput.value,
+          };
+
+          try {
+            const response = await fetch(`/events/${idInput.value}`, {
+              method: 'PUT',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify(eventData),
+            });
+
+            if (response.status === 200) {
+              const updatedEvent = await response.json();
+              updateEventInList(updatedEvent);
+              idInput.value = '';
+              timeInput.value = '';
+              activityInput.value = '';
+              locationInput.value = '';
+              notesInput.value = '';
+            } else {
+              console.error('Error updating event:', response.status, response.statusText);
+            }
+          } catch (err) {
+            console.error('Error updating event:', err);
+          }
+        });
       } else {
-        console.error('Error updating event:', response.status, response.statusText);
+        console.error('Error retrieving event:', response.status, response.statusText);
       }
     } catch (err) {
-      console.error('Error updating event:', err);
+      console.error('Error retrieving event:', err);
     }
   });
 
@@ -100,6 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Display Event in List
   const displayEvent = (event) => {
     const li = document.createElement('li');
+    li.dataset.time = event.time;
     li.innerHTML = `
       <strong>ID:</strong> ${event.id}<br>
       <strong>Time:</strong> ${event.time}<br>
@@ -109,6 +132,12 @@ document.addEventListener('DOMContentLoaded', () => {
       <button class="delete-button" data-id="${event.id}">Delete</button>
     `;
     eventList.appendChild(li);
+
+    const sortedListItems = Array.from(eventList.getElementsByTagName('li')).sort((a, b) => {
+      return a.dataset.time.localeCompare(b.dataset.time);
+    });
+    eventList.innerHTML = '';
+    sortedListItems.forEach((item) => eventList.appendChild(item));
 
     const deleteButton = li.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => deleteEvent(event.id));
@@ -125,6 +154,12 @@ document.addEventListener('DOMContentLoaded', () => {
       <strong>Notes:</strong> ${event.notes}<br>
       <button class="delete-button" data-id="${event.id}">Delete</button>
     `;
+
+    const sortedListItems = Array.from(eventList.getElementsByTagName('li')).sort((a, b) => {
+      return a.dataset.time.localeCompare(b.dataset.time);
+    });
+    eventList.innerHTML = '';
+    sortedListItems.forEach((item) => eventList.appendChild(item));
 
     const deleteButton = listItem.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => deleteEvent(event.id));
